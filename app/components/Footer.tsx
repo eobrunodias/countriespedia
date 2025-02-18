ffdc5b925ee660a4fd1f4a1958629c59ec7b521f
Bruno <brunodias40@gmail.com>
2025-02-18 13:08:11 -0300

    feat: add Header and Footer components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-6 mt-8">
+      <p className="text-center">Footer</p>
+    </footer>
+  );
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="py-6 mb-8">
+      <h1 className="font-bold">Countriespedia</h1>
+    </header>
+  );
+}
