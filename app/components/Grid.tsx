b48cbf4364bd97e45030f6c015fb32d2db42908e
Bruno <brunodias40@gmail.com>
2025-03-03 21:00:05 -0300

    feat: add Grid component

---
+++ app/components/Grid.tsx
@@ -0,0 +1,11 @@
+type GridProps = {
+  children: React.ReactNode;
+};
+
+export default function Grid({ children }: GridProps) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap 6">
+      {children}
+    </div>
+  );
+}
