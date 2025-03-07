adb99a1a26ead49393d0215b6b72e9c26bffb4ac
Bruno <brunodias40@gmail.com>
2025-03-07 09:42:42 -0300

    style: enhance Error component layout and styling for better visibility

--- app/components/Error.tsx
+++ app/components/Error.tsx
@@ -1,11 +1,17 @@
+import { XCircleIcon } from "@heroicons/react/24/outline";
+
 interface ErrorProps {
   text: string;
 }
 
 export default function Error({ text }: ErrorProps) {
   return (
-    <>
-      <p>{text}</p>
-    </>
+    <div className="flex flex-col items-center m-auto">
+      <div className="flex items-center justify-center gap-2 mb-6">
+        <XCircleIcon className="size-9" />
+        <span className="text-xl">Error</span>
+      </div>
+      <p className="text-xl">{text}</p>
+    </div>
   );
 }
